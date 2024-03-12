81b3cb319ba2b547c743b1f50320a2a20af50d86
Emanuel Mineda Carneiro <mineda@gmail.com>
2024-03-12 13:24:26 +0000

    Página de usuários

---
+++ src/components/CustomTable.vue
@@ -0,0 +1,20 @@
+<template>
+    <table>
+    <thead>
+        <td>Id</td>
+        <td>Nome</td>
+    </thead>
+    <tbody>
+        <tr v-for="usuario in usuarios">
+        <td>{{ usuario.id }}</td>
+        <td>{{ usuario.nome }}</td>
+        </tr>
+    </tbody>
+    </table>
+</template>
+  
+<script setup lang="ts">
+defineProps<{
+  usuarios: {id: number, nome: string} []
+}>()   
+</script>
---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Welcome {{ nome }}</h1>
+    <input type="text" :value="nome" @input="mudouTexto"/>
+    <p v-if="nome.length > 10">Nome longo!</p>
+    <p v-else>Nome curto!</p>
+    <CustomTable :usuarios="usuarios"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import CustomTable from '@/components/CustomTable.vue';
+
+const nome = ref("teste");
+const usuarios = ref([ {id:1, nome: "Geraldo"}, {id: 2, nome: "Cláudia"}]);
+
+function mudouTexto(e: any) {
+  nome.value = e.target.value;
+}
+
+</script>
